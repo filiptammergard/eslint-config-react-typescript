31abea622288969b65d111793ce119b4944ab5f5
Filip Tammergård <filip@tammergard.se>
2021-09-07 21:10:21 +0200

    feat: add initial config

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "@tammergard/eslint-config-base",
+    "@tammergard/eslint-config-react",
+    "@tammergard/eslint-config-typescript",
+  ],
+  rules: {
+    "react/require-default-props": "off",
+  },
+};
