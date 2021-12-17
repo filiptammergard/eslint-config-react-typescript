3d31f01228daac873c14d0cb37ebbdad07ecaff2
Filip Tammergård <filip@tammergard.se>
2021-12-17 09:49:50 +0100

    feat: remove middleman configs

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 module.exports = {
   extends: [
     "@tammergard/eslint-config-base",
-    "@tammergard/eslint-config-react",
-    "@tammergard/eslint-config-typescript",
+    "eslint-config-airbnb",
+    "plugin:eslint-plugin-react/jsx-runtime",
+    "plugin:@typescript-eslint/eslint-plugin/recommended",
   ],
   rules: {
     "react/require-default-props": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
   },
-};
+}
