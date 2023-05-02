942eec6c2fa79361dd621c0dc650462d36d4abb5
Filip Tammergård <filip@tammergard.se>
2023-05-02 15:54:12 +0200

    Enable recommended requiring type checking

---
+++ .changeset/old-hornets-reflect.md
@@ -0,0 +1,5 @@
+---
+"@tammergard/eslint-config-react-typescript": minor
+---
+
+Extend recommended requiring type checking.
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 module.exports = {
-	parser: "@typescript-eslint/parser",
 	extends: [
 		"eslint-config-airbnb",
 		"plugin:eslint-plugin-react/jsx-runtime",
 		"@tammergard/eslint-config-base",
 		"plugin:@typescript-eslint/eslint-plugin/recommended",
+		"plugin:@typescript-eslint/eslint-plugin/recommended-requiring-type-checking",
 	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+	},
 	rules: {
 		"@typescript-eslint/explicit-module-boundary-types": "off",
 		"react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
