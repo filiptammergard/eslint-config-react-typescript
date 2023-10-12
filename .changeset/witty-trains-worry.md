5bb94a5d28bcc20107a06e28123dd6e6053562e8
Filip Tammergård <filip@tammergard.se>
2023-10-12 15:31:37 +0200

    Migrate to flat config

---
+++ .changeset/witty-trains-worry.md
@@ -0,0 +1,5 @@
+---
+"@tammergard/eslint-config-react-typescript": major
+---
+
+Migrate to flat config
--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-module.exports = {
-	extends: [
-		"eslint-config-airbnb",
-		"plugin:eslint-plugin-react/jsx-runtime",
-		"@tammergard/eslint-config-base",
+const { FlatCompat } = require("@eslint/eslintrc")
+const typescriptParser = require("@typescript-eslint/parser")
+const tammergardBaseConfig = require("@tammergard/eslint-config-base")
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+})
+
+module.exports = [
+	...compat.extends("eslint-config-airbnb"),
+	...compat.extends("plugin:eslint-plugin-react/jsx-runtime"),
+	...compat.extends(
 		"plugin:@typescript-eslint/eslint-plugin/recommended-type-checked",
+	),
+	...compat.extends(
 		"plugin:@typescript-eslint/eslint-plugin/stylistic-type-checked",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		project: true,
+	),
+	tammergardBaseConfig,
+	{
+		files: ["**/*.{ts,tsx}"],
+		rules: {
+			"@typescript-eslint/array-type": ["error", { default: "generic" }],
+			"@typescript-eslint/explicit-module-boundary-types": "off",
+			"react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+			"react/jsx-props-no-spreading": "off",
+			"react/require-default-props": "off",
+		},
 	},
-	rules: {
-		"@typescript-eslint/array-type": ["error", { default: "generic" }],
-		"@typescript-eslint/explicit-module-boundary-types": "off",
-		"react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
-		"react/jsx-props-no-spreading": "off",
-		"react/require-default-props": "off",
+	{
+		languageOptions: {
+			parser: typescriptParser,
+			parserOptions: {
+				project: true,
+			},
+		},
 	},
-}
+]
